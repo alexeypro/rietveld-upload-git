0e7e621eb7e14d098d594fc841993820298ba727
Alexey Prohorenko <me@alexeypro.com>
2011-08-31 11:52:25 -0700

    assume answer yes

--- upload-git.sh
+++ upload-git.sh
@@ -23,4 +23,4 @@ GIT_BRANCH=`$GIT_BIN name-rev --name-only HEAD`
 #GIT_BRANCH=`$GIT_BIN branch --no-color 2> /dev/null | sed -e '/^[^*]/d' | cut -f 2 -d' '`
 #GIT_PROJECT=`cat .git/FETCH_HEAD | cut -f 2 -d"/"`
 BASE_URL="https://github.com/$GIT_ACCOUNT_NAME/$GIT_PROJECT/tree/$GIT_BRANCH"
-$PYTHON_BIN $UPLOAD_PY --rev=$GIT_BRANCH -s $SERVER $USERNAME_TO_USE --account_type=$SERVER_TYPE -r $REVIEWERS_EMAILS --send_mail --base_url=$BASE_URL -m "$MESSAGE_ISSUE"
+$PYTHON_BIN $UPLOAD_PY --rev=$GIT_BRANCH -s $SERVER $USERNAME_TO_USE --account_type=$SERVER_TYPE -r $REVIEWERS_EMAILS --send_mail --base_url=$BASE_URL --assume_yes -m "$MESSAGE_ISSUE"
